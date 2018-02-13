a2683e02e58a370f3048f390fb0f6cbef5ff1acf
Matthew Harwood <matthhar12@gmail.com>
2018-02-12 23:41:30 -0800

    - added the higherorder fb serve.

--- apps/morningharwood/src/app/app.component.ts
+++ apps/morningharwood/src/app/app.component.ts
@@ -1,20 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
+import { HigherOrderFirestoreService } from './firestore/services/ho-firestore.service';
+
+
+interface Note {
+  content: string;
+  size: string;
+}
 
 @Component({
   selector: 'mh-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [ './app.component.scss' ],
 })
 export class AppComponent implements OnInit {
   private notesCollection: AngularFirestoreCollection<any>;
-  public notes: Observable<any[]>;
+  // public notes: Observable<any[]>;
+  public ref: AngularFirestoreCollection<Note>;
+  public notes: Observable<Note[]>;
 
-  constructor(private afs: AngularFirestore) {}
+  constructor(private afs: AngularFirestore, private hoAfs: HigherOrderFirestoreService) {}
 
   ngOnInit() {
-    this.notesCollection = this.afs.collection('notes');
-    this.notes = this.notesCollection.valueChanges();
+    // this.notesCollection = this.afs.collection('notes');
+    // this.notes = this.notesCollection.valueChanges();
+    console.log(this.hoAfs.inspectCol('notes'));
+
   }
 }
--- apps/morningharwood/src/app/app.module.ts
+++ apps/morningharwood/src/app/app.module.ts
@@ -5,26 +5,17 @@ import { RouterModule } from '@angular/router';
 import { NxModule } from '@nrwl/nx';
 import { aclRoleRoutes } from '@suite/acl-role';
 import { AppComponent } from './app.component';
-import { MhFirestoreClientModule } from './configs/firestore/mh-client.module';
-import { AngularFirestoreModule } from 'angularfire2/firestore';
-import { AngularFireModule } from 'angularfire2';
-import { MhClientFirestore } from './configs/firestore/mh-client';
+import { MhFirestoreClientModule } from './firestore/mh-client.module';
 
 
 @NgModule({
   imports: [
     CommonModule,
-
     BrowserModule,
     NxModule.forRoot(),
-    MhFirestoreClientModule,
+    MhFirestoreClientModule.forRoot(),
     RouterModule.forRoot(
-      [
-        {
-          path: 'acl-role',
-          children: aclRoleRoutes,
-        },
-      ],
+      [],
       { initialNavigation: 'enabled' },
     ),
   ],
