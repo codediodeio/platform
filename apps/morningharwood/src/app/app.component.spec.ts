ea9027924cd8a0d72e0a14e65339dbf9caccf783
Matthew Harwood <matthhar12@gmail.com>
2018-02-12 22:18:26 -0800

    - all tests passing with firestore.

--- apps/morningharwood/src/app/app.component.spec.ts
+++ apps/morningharwood/src/app/app.component.spec.ts
@@ -1,10 +1,39 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
-import { MhFirestoreClientModule } from './configs/firestore/mh-client.module';
-import { AngularFirestore, AngularFirestoreModule } from 'angularfire2/firestore';
+import { FirebaseFirestore } from '@firebase/firestore-types';
+import { FirebaseApp } from 'angularfire2';
+import {
+  AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument,
+  QueryFn,
+} from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+import { AppComponent } from './app.component';
 
+
+class FirebaseMock implements AngularFirestore {
+  app: FirebaseApp;
+  firestore: FirebaseFirestore;
+  persistenceEnabled$: Observable<boolean>;
+
+  collection<T>(path: string, queryFn?: QueryFn): any {
+    return {
+      valueChanges: () => ''
+    }
+  }
+
+  doc<T>(path: string): AngularFirestoreDocument<T> {
+    return undefined;
+  }
+
+  createId(): string {
+    return undefined;
+  }
+
+  private valueChanges() {
+    return Observable.of(true);
+  }
+}
 
 describe('AppComponent', () => {
   let component: AppComponent;
@@ -17,9 +46,15 @@ describe('AppComponent', () => {
           RouterTestingModule,
         ],
         declarations: [ AppComponent ],
-
+        providers: [
+          {
+            provide: AngularFirestore,
+            useClass: FirebaseMock,
+          },
+        ],
       }).compileComponents();
     }),
+    // spyOn(FirebaseMock, 'collection')
   );
 
   beforeEach(() => {
